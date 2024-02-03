a72ae33a29772e052945cdc7fd7794bf467ce7fd
Diego Cortes <cortesmendez.2003@gmail.com>
2024-02-02 18:11:47 -0600

    inicio 1.1

---
+++ products.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RLsolutions</title>
+</head>
+<body>
+    <h1>Mostrador de datos</h1>
+</body>
+</html>
